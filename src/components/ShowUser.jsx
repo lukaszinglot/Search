7cbd0a590faeda7dc5ea262a098bd4bb859fd69a
lukaszinglot <luk.inglot@gmail.com>
2019-12-10 21:36:45 +0100

    add component which show users on the screen, and have search bar

---
+++ src/components/ShowUser.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getUsers, getSearch } from "../store/actions/userActions";
+import showUserStyles from "./showUser.module.scss";
+
+class ShowUser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: ""
+    };
+
+    this.updateSearch = this.updateSearch.bind(this);
+  }
+
+  componentDidMount() {
+    if (!this.props.users.length) {
+      this.props.getUsers();
+    }
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    const { search } = this.state;
+    const { getSearch } = this.props;
+    if (search !== nextState.search) {
+      getSearch(nextState.search);
+    }
+  }
+
+  renderUsers() {
+    const { users, search } = this.props;
+    if (search.length) {
+      return search.map(({ id, name, username }, index) => {
+        return <div key={id}>{`${index + 1}. ${name}  @${username}`}</div>;
+      });
+    } else {
+      return users.map(({ id, name, username }, index) => {
+        return <div key={id}>{`${index + 1}. ${name}  @${username}`}</div>;
+      });
+    }
+  }
+
+  updateSearch(event) {
+    this.setState({
+      search: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className={showUserStyles.user}>
+        <h1>Users List</h1>
+        <input
+          type="text"
+          placeholder="Search by username"
+          onChange={this.updateSearch}
+          value={this.state.search}
+          className={showUserStyles.inputUser}
+        />
+        {this.renderUsers()}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    users: state.usersReducer.data,
+    search: state.usersReducer.filtered
+  };
+};
+
+export default connect(mapStateToProps, {
+  getUsers,
+  getSearch
+})(ShowUser);
