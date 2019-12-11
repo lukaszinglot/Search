282a9bdc39e0747936afe01b33885fddc856232e
lukaszinglot <luk.inglot@gmail.com>
2019-12-11 15:00:44 +0100

    connect Redux to UserList component

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import userListStyles from "./userList.module.scss";
+import { connect } from "react-redux";
+import { getFilteredUsers } from "../store/selectors/getFilteredUsers";
 
-const UserList = props => {
-  return props.userList.map(({ id, name, username }, index) => {
-    return (
-      <div key={id}>
-        <span className={userListStyles.username}>{`${index + 1}.`}</span>
-        {name}
-        <span className={userListStyles.username}>{` @${username}`}</span>
-      </div>
-    );
-  });
+const UserList = ({ userList }) => {
+  if (userList) {
+    return userList.map(({ id, name, username }, index) => {
+      return (
+        <div key={id}>
+          <span className={userListStyles.username}>{`${index + 1}.`}</span>
+          {name}
+          <span className={userListStyles.username}>{` @${username}`}</span>
+        </div>
+      );
+    });
+  }
 };
 
-export default UserList;
+const mapStateToProps = state => {
+  return {
+    userList: getFilteredUsers(state.userReducer.data, state.userReducer.text)
+  };
+};
+
+export default connect(mapStateToProps, null)(UserList);
