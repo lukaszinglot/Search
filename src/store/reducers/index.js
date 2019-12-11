0dd18df1b819e78333dc05a2a71668f6f0f72b77
lukaszinglot <luk.inglot@gmail.com>
2019-12-11 15:02:46 +0100

    remove searchReducer from index.jsx

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
 
 import userReducer from "./usersReducer";
-import searchReducer from "./searchReducer";
 
 export default combineReducers({
-  userReducer,
-  searchReducer
+  userReducer
 });
