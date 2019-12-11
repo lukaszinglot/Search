7705d5339a9be276e5176120de4a061027984ed2
lukaszinglot <luk.inglot@gmail.com>
2019-12-11 18:47:22 +0100

    Delete previous selector

--- src/store/selectors/getFilteredUsers.jsx
+++
@@ -1,7 +0,0 @@
-export const getFilteredUsers = (users, text) => {
-  const userInput = text.toLowerCase();
-
-  return users.filter(({ name }) => {
-    return name.toLowerCase().includes(userInput.toLowerCase());
-  });
-};
