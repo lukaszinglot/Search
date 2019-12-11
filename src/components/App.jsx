b313619133719b61225affd750e0ca9bd4f63cde
lukaszinglot <luk.inglot@gmail.com>
2019-12-11 12:37:38 +0100

    Change ShowUser to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,42 @@
-import React from "react";
-import ShowUser from "./ShowUser";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getUsers } from "../store/actions/userActions";
+import showUserStyles from "./showUser.module.scss";
+import UserSearch from "./UserSearch";
+import { getFilteredUsers } from "../store/selectors/getFilteredUsers";
+import UserList from "./UserList";
 
-const App = () => {
-  return (
-    <div>
-      <ShowUser />
-    </div>
-  );
+class ShowUser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const { users } = this.props;
+    if (!users.length) {
+      this.props.getUsers();
+    }
+  }
+
+  render() {
+    const { users } = this.props;
+    return (
+      <div className={showUserStyles.user}>
+        <h1>Users list</h1>
+        <UserSearch />
+        <UserList userList={users} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    users: getFilteredUsers(state.userReducer.data, state.searchReducer)
+  };
 };
 
-export default App;
+export default connect(mapStateToProps, {
+  getUsers
+})(ShowUser);
